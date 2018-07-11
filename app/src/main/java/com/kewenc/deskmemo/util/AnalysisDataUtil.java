6b67e8e3a911f6e2dd5883a97d187fba97529d80
KewenC <KewenC@outlook.com>
2018-07-11 13:36:28 +0800

    创建数据原型

---
+++ app/src/main/java/com/kewenc/deskmemo/util/AnalysisDataUtil.java
@@ -0,0 +1,10 @@
+package com.kewenc.deskmemo.util;
+
+public class AnalysisDataUtil {
+
+//        <kctxt>Hello Desk Memo !</kctxt>
+//        <kcimg>/Android/com/kewenc/deskmemo/img01</kcimg>
+//        <kcvoi>/Android/com/kewenc/deskmemo/voi01</kcvoi>
+//        <kcvid>/Android/com/kewenc/deskmemo/vid01</kcvid>
+
+}
